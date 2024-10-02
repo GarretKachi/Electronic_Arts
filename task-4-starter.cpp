bdd27a1a2aea1019c41afad7f7f170faab1f12e6
Garret Akpale <garretakpale@gmail.com>
2024-10-02 01:37:53 -0700

    edited remove item function

--- task-4-starter.cpp
+++ task-4-starter.cpp
@@ -94,23 +94,39 @@ public:
     }
 
     void remove_item(int item_index) {
-        int input_quantity;
-        Item *item = items[item_index];
-        std::cout << "\nEnter number of items to sell: ";
-        std::cin >> input_quantity;
-
-        int quantity = item->get_quantity();
-        if (input_quantity <= quantity) {
-            float price = item->get_price();
-            float money_earned = price * input_quantity;
-            item->set_quantity(quantity - input_quantity);
-            std::cout << "\nItems sold";
-            std::cout << "\nMoney received: " << money_earned;
-            total_money += money_earned;
-        } else {
-            std::cout << "\nCannot sell more items than you have.";
-        }
+	int input_quantity;
+	Item *item = items[item_index];
+	std::cout << "\nEnter number of items to sell: ";
+	std::cin >> input_quantity;
+	
+	int quantity = item->get_quantity();
+	if (input_quantity <= quantity) {
+	    float price = item->get_price();
+	    float money_earned = price * input_quantity;
+	    item->set_quantity(quantity - input_quantity);
+	    std::cout << "\nItems sold";
+	    std::cout << "\nMoney received: " << money_earned;
+	    total_money += money_earned;
+	    
+	    // If quantity is now zero, remove the item from inventory
+	    if (item->get_quantity() == 0) {
+		std::cout << "\nItem quantity is zero, removing item from inventory.";
+		delete item; // Free memory
+		
+		// Shift all items after the removed one to the left
+		for (int i = item_index; i < item_count - 1; i++) { 
+		     items[i] = items[i + 1];
+		}
+		
+		items[item_count - 1] = nullptr; // Set the last item to nullptr after shifting
+		item_count--; // Decrease the item count
+	    }
+	
+	} else {
+	     std::cout << "\nCannot sell more items than you have.";
+	}
     }
+
 
     void list_items() {
         if (item_count == 0) {
